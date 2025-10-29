b2206fc9d7c9990903a6e9bf73553875618b2436
murphy-afk <bardarenoemi@gmail.com>
2025-10-29 15:25:13 +0100

    feat: arrays 01

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,24 +10,49 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+const fourthTeacher = teachers[3];
+console.log(fourthTeacher);
+
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers[4] = "Patrick";
+console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+
+const lastTeacher = teachers[teachers.length - 1];
+teachers.pop();
+console.log(lastTeacher);
+
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers[0];
+teachers.shift();
+console.log(firstTeacher);
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
+teachers.push("Vanessa");
+console.log(teachers);
+
+
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+
+teachers.unshift("Sarah");
+console.log(teachers);
+
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf("Lewis");
+console.log(lewisIndex);
+
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+let isTeachersEmpty = false;
+if (teachers.length === 0) {
+  isTeachersEmpty = true;
+}
+console.log(isTeachersEmpty);
